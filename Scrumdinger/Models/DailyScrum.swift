f8b0fdb837fd62bae75273e6adc8e1bc1d411ab1
christinetrac <christineetrac@gmail.com>
2022-01-15 02:52:28 -0500

    scrums view completed

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,18 +7,27 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
         DailyScrum(title: "Design", attendees: ["Rachel", "Daisy", "Simon"], lengthInMinutes: 10, theme: .lavender),
-        DailyScrum(title: "Frontend Dev", attendees: ["Christine", "Darla", "Katie", "Euna"], lengthInMinutes: 5, theme: .seafoam),
-        DailyScrum(title: "Backend Dev", attendees: ["Andrew", "Jacob", "Chad", "Eden", "Jen"], lengthInMinutes: 5, theme: .buttercup)
+        DailyScrum(title: "Frontend Dev", attendees: ["Christine", "Darla", "Katie", "Euna"], lengthInMinutes: 5, theme: .buttercup),
+        DailyScrum(title: "Backend Dev", attendees: ["Andrew", "Jacob", "Chad", "Eden", "Jen"], lengthInMinutes: 5, theme: .seafoam)
     ]
 }
